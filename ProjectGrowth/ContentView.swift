664d88fd7b86686ea256643667ee0363a9e795e2
PhraxayaM <mphraxa@gmail.com>
2020-05-19 07:39:55 -0700

    updated readme

---
+++ ProjectGrowth/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  ProjectGrowth
+//
+//  Created by MattHew Phraxayavong on 5/18/20.
+//  Copyright © 2020 MattHew Phraxayavong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack
+            {
+            ZStack
+                {
+            HStack
+                {
+                    Text("Hello, World!")
+
+                
+            }
+            }
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ProjectGrowth
+# Project Growth
+![Imgur Image](https://imgur.com/h8F0a7P)
+
+Project Growth is designed for people who want to keep a progress track of something new they started, such as growing a new plant.
+
+## Wireframe
+
+## Planned Features
+
+- [] Start a new growth project with stats added
+- [] Share to social media
+- [] Add video support
+
